ca12c49bd7cb390f37b2479b6bcc866eccb92364
suntala <arati.rana@grafana.com>
2023-12-06 15:34:39 +0100

    Fix panel table migration, dashboard uid type

--- pkg/services/sqlstore/migrations/panel_mig.go
+++ pkg/services/sqlstore/migrations/panel_mig.go
@@ -18,7 +18,7 @@ func addPanelMigrations(mg *migrator.Migrator) {
 				OrgIDinPanelTable)).
 		Mysql(
 			fmt.Sprintf(
-				"CREATE TABLE panel (title text, %s int, %s int);",
+				"CREATE TABLE panel (title varchar(255), %s varchar(255), %s int) COLLATE utf8mb4_unicode_ci;",
 				DashUIDinPanelTable,
 				OrgIDinPanelTable)).
 		Postgres(
